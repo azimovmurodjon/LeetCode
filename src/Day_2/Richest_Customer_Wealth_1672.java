3fa657f5a8621d14b7b466bf79ca153d39a8564c
azimovmurodjon <azimovm.j@gmail.com>
2024-04-26 00:51:48 -0400

    Richest_Customer_Wealth_1672 is added

---
+++ src/Day_2/Richest_Customer_Wealth_1672.java
@@ -0,0 +1,23 @@
+package Day_2;
+
+public class Richest_Customer_Wealth_1672 {
+    public int maximumWealth(int[][] accounts){
+        int maxWealthSoFar = 0;
+
+        for(int[] customer : accounts){
+            int currentCustomerWelth = 0;
+
+            for(int bank : customer){
+                currentCustomerWelth += bank;
+            }
+
+            maxWealthSoFar = Math.max(maxWealthSoFar, currentCustomerWelth);
+        }
+        return maxWealthSoFar;
+    }
+}
+
+/**
+ * time complexity = 0(n x m)
+ * space complexity = 0(1)
+ */
