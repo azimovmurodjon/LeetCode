8de231b8d9ebeb889c5a9a7e092385bc72ebf6f5
azimovmurodjon <azimovm.j@gmail.com>
2024-04-28 16:38:28 -0400

    Middle_of_the_Linked_List_876 added to day 4

---
+++ src/Day_4/Middle_of_the_Linked_List_876.java
@@ -0,0 +1,54 @@
+/**
+ * Given the head of a singly linked list, return the middle node of the linked list.
+ *
+ * If there are two middle nodes, return the second middle node.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [1,2,3,4,5]
+ * Output: [3,4,5]
+ * Explanation: The middle node of the list is node 3.
+ * Example 2:
+ *
+ *
+ * Input: head = [1,2,3,4,5,6]
+ * Output: [4,5,6]
+ * Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the list is in the range [1, 100].
+ * 1 <= Node.val <= 100
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+package Day_4;
+
+import java.util.ArrayList;
+
+public class Middle_of_the_Linked_List_876 {
+//    public ListNode middleNode(ListNode head) {
+//        ArrayList<ListNode> array = new ArrayList<ListNode>();
+//
+//        int lenght = 0;
+//        while(head != null){
+//            array.add(head);
+//            head = head.next;
+//            lenght++;
+//        }
+//        return array.get(lenght / 2);
+//    }
+}
