0e2b6351a5b325037ccc4cd57cdf2d1d91df2132
azimovmurodjon <azimovm.j@gmail.com>
2024-05-08 00:07:20 -0400

    Same_Tree_100 and Same_Tree_100.txt file has been added to Day_13

---
+++ src/Week_2/Day_14/Same_Tree_100.java
@@ -0,0 +1,12 @@
+package Week_2.Day_14;
+
+public class Same_Tree_100 {
+//    public boolean isSameTree(TreeNode p, TreeNode q) {
+//        if (p == null && q == null) return true;
+//        if (p == null || q == null) return false;
+//
+//        return p.val == q.val &&
+//                isSameTree(p.left, q.left) &&
+//                isSameTree(p.right, q.right);
+//    }
+}
