6b70434e2edae89eb8bdc3614b4ad7fd8130b309
azimovmurodjon <azimovm.j@gmail.com>
2024-05-09 11:02:03 -0400

    Maximum_Depth_Binary_Tree_104 and Maximum_Depth_Binary_Tree_104.txt file has been added to Day_13

---
+++ src/Week_3/Day_15/Maximum_Depth_Binary_Tree_104.java
@@ -0,0 +1,23 @@
+package Week_3.Day_15;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Maximum_Depth_Binary_Tree_104 {
+//    public int maxDepth(TreeNode root) {
+//        if(root == null) return 0;
+//
+//        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+//    }
+}
