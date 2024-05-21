148a196e0e545e1b649cca603c0499fe2feda942
azimovmurodjon <azimovm.j@gmail.com>
2024-05-21 11:41:44 -0400

    Binary_Tree_Inorder_Traversal_94 added to Day_24

---
+++ src/Week_4/Day_24/Binary_Tree_Inorder_Traversal_94.java
@@ -0,0 +1,26 @@
+package Week_4.Day_24;
+
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Binary_Tree_Inorder_Traversal_94 {
+//    public List<Integer> inorderTraversal(TreeNode root) {
+//        List<Integer> res = new ArrayList<>();
+//        dfs(res, root);
+//        return res;
+//    }
+//
+//    void dfs(List<Integer> res, TreeNode node) {
+//        if (node != null) {
+//            if (node.left != null) {
+//                dfs(res, node.left);
+//            }
+//            res.add(node.val);
+//            if (node.right != null) {
+//                dfs(res, node.right);
+//            }
+//        }
+//    }
+}
+
