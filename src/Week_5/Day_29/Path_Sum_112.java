c7bd8bd549bdfc5c0bd23fbc2a8f6e90d30c1f1a
azimovmurodjon <azimovm.j@gmail.com>
2024-05-29 14:24:10 -0400

    Path_Sum_112 and Path_Sum_112.txt added to Day_28

---
+++ src/Week_5/Day_29/Path_Sum_112.java
@@ -0,0 +1,20 @@
+package Week_5.Day_29;
+
+public class Path_Sum_112 {
+//    public boolean hasPathSum(TreeNode root, int targetSum) {
+//        return hasPathSum(root, 0, targetSum);
+//    }
+//
+//    private boolean hasPathSum(TreeNode node, int sum, int targetSum) {
+//        if (node == null) return false;
+//
+//        sum += node.val;
+//
+//        if (node.left == null && node.right == null) {
+//            return sum == targetSum;
+//        }
+//
+//        return hasPathSum(node.left, sum, targetSum) ||
+//                hasPathSum(node.right, sum, targetSum);
+//    }
+}
