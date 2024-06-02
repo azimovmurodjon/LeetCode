69da63c2448550b682f5db361be5e6debe7facc9
azimovmurodjon <azimovm.j@gmail.com>
2024-06-02 11:19:20 -0400

    Happy_Number_202 and Reverse_Bits_190.Happy_Number_202 added to Day_32

---
+++ src/Week_5/Day_32/Happy_Number_202.java
@@ -0,0 +1,26 @@
+package Week_5.Day_32;
+
+public class Happy_Number_202 {
+    public boolean isHappy(int n) {
+        int slow = n, fast = n;
+
+        while (slow != 1) {
+            slow = sumOfSquareOfDigits(slow);
+            fast = sumOfSquareOfDigits(sumOfSquareOfDigits(fast));
+
+            if(slow == fast) break;
+        }
+        return  slow == 1;
+
+    }
+    private int sumOfSquareOfDigits(int x){
+        int sum = 0;
+
+        while (x > 0){
+            int d = x % 10;
+            sum += (d * d);
+            x /= 10;
+        }
+        return sum;
+    }
+}
