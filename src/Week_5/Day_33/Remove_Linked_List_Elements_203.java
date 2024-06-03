97700939b1758aa6158e60c409388c61f3aaaa2c
azimovmurodjon <azimovm.j@gmail.com>
2024-06-03 11:07:46 -0400

    Remove_Linked_List_Elements_203 and Remove_Linked_List_Elements_203.txt added to Day_33

---
+++ src/Week_5/Day_33/Remove_Linked_List_Elements_203.java
@@ -0,0 +1,21 @@
+package Week_5.Day_33;
+
+public class Remove_Linked_List_Elements_203 {
+//    public ListNode removeElements(ListNode head, int val) {
+//        ListNode sHead = new ListNode(), sTail = sHead, node = head;
+//
+//        while (node != null) {
+//            ListNode next = node.next;
+//            node.next = null;
+//
+//            if (node.val != val) {
+//                sTail.next = node;
+//                sTail = node;
+//            }
+//
+//            node = next;
+//        }
+//
+//        return sHead.next;
+//    }
+}
