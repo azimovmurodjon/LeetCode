073428a312a58d64c104bd3be22b3bb417b6ffd2
azimovmurodjon <azimovm.j@gmail.com>
2024-07-30 22:23:08 -0400

    Sort_the_People_2418 added to Day_66

--- src/Week_10/Day_66/Sort_the_People_2418.java
+++ src/Week_10/Day_66/Sort_the_People_2418.java
@@ -14,7 +14,6 @@ public class Sort_the_People_2418 {
         for (int i = 0; i < numberOfPeople; ++i) {
             indices[i] = i;
         }
-
         // Sort the indices array based on the descending order of heights using a custom comparator.
         // The comparator computes the difference between the heights at indices j and i.
         Arrays.sort(indices, (i, j) -> heights[j] - heights[i]);
